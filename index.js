c0e070cfc7a15f6c19d914a4209e5a3f503346fa
Tanishqua24 <91310415+Tanishqua24@users.noreply.github.com>
2021-11-28 23:23:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+function pr() {
+    var e1 = document.getElementById("one");
+    e1.innerText = "Good Morning";
+    e1.style.color = "black";
+    var e2 = document.getElementById("two");
+    e2.innerText = "Tanishqua";
+    e2.style.color = "white";
+}
+
+function br() {
+    var e1 = document.getElementById("one");
+    e1.innerText = "Good Evening";
+    e1.style.color = "pink";
+    var e2 = document.getElementById("two");
+    e2.innerText = "Tanishqua";
+    e2.style.color = "purple";
+}
+
+function cr() {
+    var e1 = document.getElementById("one");
+    e1.innerText = "Good Night";
+    e1.style.color = "grey";
+    var e2 = document.getElementById("two");
+    e2.innerText = "Tanishqua";
+    e2.style.color = "violet";
+}
